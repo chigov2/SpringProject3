c890f15117e67f39188ae8d55734262f4bcbd5bd
mikee <chigov@gmail.com>
2023-08-30 11:09:56 +0300

    19

--- src/spring_introduction/Test7_ConfigWithAnnotation1.java
+++ src/spring_introduction/Test7_ConfigWithAnnotation1.java
@@ -10,6 +10,9 @@ public class Test7_ConfigWithAnnotation1 {
         Person person = context.getBean("personBean", Person.class);
         person.callMyPet();
 
+        System.out.println(person.getSurname());
+        System.out.println(person.getAge());
+
 //        Cat cat = context.getBean("cat", Cat.class);
 //        cat.say();
         context.close();
